ecf3620e67fdb6530d0d15b8bd213766232c5a5a
NathanGrenier <nathangrenier01@gmail.com>
2023-06-20 17:37:09 -0400

    Assignment 4 initial commit

---
+++ Assignment4/src/ca/concordia/comp352/hashing/Hashomatic.java
@@ -0,0 +1,9 @@
+package ca.concordia.comp352.hashing;
+
+import main.stuff.MyHash;
+
+public class Hashomatic extends MyHash{
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
